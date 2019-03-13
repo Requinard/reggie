816bddd99d5e74c413fed312dc1b651742438290
requinard <david@requinard.nl>
2019-03-13 16:29:32 +0100

    model registration process

--- reggie-server/registration/admin.py
+++ reggie-server/registration/admin.py
@@ -1,3 +1,88 @@
 from django.contrib import admin
+from registration import models
+
+metadata_fields = ("Metadata", {
+    'fields': ('date_created', 'date_edited', 'uuid'),
+    'classes': ['collapse', ]
+})
+
 
 # Register your models here.
+class PaymentInline(admin.StackedInline):
+    model = models.PaymentModel
+
+    verbose_name = "Payment"
+    verbose_name_plural = "Payments"
+    classes = ['collapse', ]
+
+    fields = ('registration', ('amount', 'date_registered'))
+
+
+class RegistrationAddinInline(admin.StackedInline):
+    model = models.RegistrationAddinModel
+
+    verbose_name = "Registration Addin"
+    verbose_name_plural = "Registration Addins"
+    classes = ["collapse", ]
+
+
+@admin.register(models.ConventionModel)
+class ConventionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'con_start_time', 'con_reg_time', 'price', 'date_edited')
+    list_filter = ('con_start_time', 'con_reg_time', 'date_edited', 'date_created')
+    inlines = [
+        RegistrationAddinInline
+    ]
+
+    fieldsets = (
+        ("Con Info", {
+            "fields": ('name', 'con_start_time', 'con_reg_time', 'price')
+        }),
+        metadata_fields,
+    )
+    readonly_fields = ('date_created', 'date_edited', 'uuid')
+
+
+@admin.register(models.RegistrationModel)
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'convention', 'is_accepted', 'is_pending')
+    list_filter = ('convention', 'is_accepted', 'is_pending', 'date_edited')
+    inlines = [
+        PaymentInline
+    ]
+    fieldsets = (
+        ("Attendee Info", {
+            'fields': ('user', 'convention')
+        }),
+        ("Registration Status", {
+            'fields': ('is_accepted', 'is_pending')
+        }),
+        metadata_fields,
+    )
+
+    readonly_fields = ('date_created', 'date_edited', 'uuid')
+
+
+@admin.register(models.PaymentModel)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('registration', 'amount', 'date_registered')
+    list_filter = ('date_registered', 'date_edited')
+
+
+@admin.register(models.RegistrationAddinModel)
+class RegistrationAddinAdmin(admin.ModelAdmin):
+    list_display = ('convention', 'name', 'price')
+    list_filter = ('convention', 'date_edited')
+    search_fields = ('name',)
+
+    fieldsets = (
+        ('Information', {
+            'fields': ('name', 'description', 'price')
+        }),
+        ("Relations", {
+            'fields': ('convention', 'registrations_added_to')
+        }),
+        metadata_fields
+    )
+
+    readonly_fields = ('date_created', 'date_edited', 'uuid')
--- reggie-server/registration/models.py
+++ reggie-server/registration/models.py
@@ -1,3 +1,84 @@
+import uuid as uuid
+from datetime import datetime
 from django.db import models
+from django.contrib.auth import models as user
+
 
 # Create your models here.
+class AbstractModel(models.Model):
+    """
+    Holds data that all classes should use
+    """
+    # Metadata
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_edited = models.DateTimeField(auto_now=True)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class ConventionModel(AbstractModel):
+    """
+    Holds the data for a specific instance of a convention.
+    """
+    name = models.CharField(max_length=200)
+    con_start_time = models.DateTimeField()
+    con_reg_time = models.DateTimeField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def reg_is_open(self):
+        return datetime.utcnow() >= self.con_reg_time
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.__unicode__()
+
+
+class RegistrationModel(AbstractModel):
+    """
+    Connects a User to a Convention and adds reg metadata
+    """
+    user = models.ForeignKey(user.User, on_delete=models.CASCADE)
+    convention = models.ForeignKey(ConventionModel, on_delete=models.CASCADE)
+    is_accepted = models.BooleanField(default=False)
+    is_pending = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if self.is_accepted:
+            self.is_pending = False
+
+        super(RegistrationModel, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.__unicode__()
+
+    def __unicode__(self):
+        return "{0} @ {1} ({2})".format(self.user, self.convention, self.uuid)
+
+
+class PaymentModel(AbstractModel):
+    """
+    Registers a payment for a RegistrationModel
+    """
+    registration = models.ForeignKey(RegistrationModel, on_delete=models.CASCADE, related_name="payments")
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date_registered = models.DateTimeField()
+    notes = models.TextField
+
+
+class RegistrationAddinModel(AbstractModel):
+    """
+    Extra items that can be added to a registration
+    """
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    registrations_added_to = models.ManyToManyField(RegistrationModel, related_name="addons", blank=True)
+    convention = models.ForeignKey(ConventionModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0} @ {1} ({2})".format(self.name, self.convention, self.uuid)
