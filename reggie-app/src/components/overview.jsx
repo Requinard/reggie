d796591c82844ec2e413ad3a14044c3494c307ac
requinard <david@requinard.nl>
2019-03-14 15:43:40 +0100

    start work with react and redux

---
+++ reggie-app/src/components/overview.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Overview extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>hi</h1>
+      </div>
+    );
+  }
+}
+
+export default Overview
---
+++ reggie-app/src/reducers/login.js
@@ -0,0 +1,8 @@
+export function login(state = {
+  isLoggedIn: false
+}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
