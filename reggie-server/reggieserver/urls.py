2283b5641111faa564fb08a1459f64539eaa243e
requinard <david@requinard.nl>
2019-03-27 15:34:51 +0100

    start staff API

--- reggie-server/reggieserver/urls.py
+++ reggie-server/reggieserver/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('stats/', include('django_prometheus.urls')),
-    url('api/', include('api.urls'))
+    url('api/', include('api.urls')),
+    url('staff/', include('staff.urls'))
 ]
